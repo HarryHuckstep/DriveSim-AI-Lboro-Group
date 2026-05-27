18d3a227a6c8627fd950841b9823361f6061ab7d
1JAGs1 <160478597+1JAGs1@users.noreply.github.com>
2026-05-27 18:51:29 +0100

    chemical send

--- src/features/efficiency.py
+++ src/features/efficiency.py
@@ -1,17 +1,45 @@
 from __future__ import annotations
 import pandas as pd
 
+# def add_mdot_air_kgs(df):
+#     df["Air Flow Rate from Mass Flow Sensor [kg/s]"] = (df["Air Flow Rate from Mass Flow Sensor [g/s]"] / 1000)
+    
+#     return df
+
+# def add_mdot_fuel(df, AFR = 14.7):
+#     df["mdot_fuel"] = df["Air Flow Rate from Mass Flow Sensor [kg/s]"]  / AFR
+
+#     return df
+
+# def add_chemical_power(df, LHV = 43e6):
+#     df["Pfuel"]  = df["mdot_fuel"] * LHV
+
+
+# def add_chemical_efficiency(df):
+#     add_mdot_air_kgs(df)
+#     add_mdot_fuel(df)
+#     add_chemical_power(df)
+#     df["chemical_efficiency"] = df["P_drive_W"] / df["Pfuel"]
+
+#     return df
+
+
 def add_mdot_air_kgs(df):
-    df["Air Flow Rate from Mass Flow Sensor [kg/s]"] = (df["Air Flow Rate from Mass Flow Sensor [g/s]"] / 1000)
+    df["maf_kgps"] = (df["maf_gps"] / 1000)
+    
     return df
 
 def add_mdot_fuel(df, AFR = 14.7):
-    df["mdot_fuel"] = df["Air Flow Rate from Mass Flow Sensor [kg/s]"]  / AFR
+    df["mdot_fuel"] = df["maf_kgps"]  / AFR
 
     return df
 
 def add_chemical_power(df, LHV = 43e6):
-    df["Pfuel"]  = df["mdot_fuel"] * LHV
+    df["Pfuel"]  = df["mdot_fuel"] * LHV 
+    valid_fuel = df["Pfuel"] > 5000
+    valid = (valid_fuel)
+    
+    return df.where(valid)
 
 
 def add_chemical_efficiency(df):
